f437a595576b993f529e8206274460caa314bc0b
Iván Araolaza <ivanaraolaza@gmail.com>
2022-01-05 09:32:58 -0300

    Fixed: now it counts the zeros of the bytes in the address all right

--- subnetting.py
+++ subnetting.py
@@ -13,84 +13,56 @@ def run():
     -------------------------------------
     """)
     print("What do you have? ")
-    #while True:
-    which_conv = input('Select H for hosts or M for masks \n')
+    while True:
+        which_conv = input('Select H for hosts or M for masks\n')
 
-    if which_conv.startswith(('m', 'M')):
+        if which_conv.startswith(('m', 'M')):
 
-        mask_address = input("Enter the mask \n") + "."
-        address_bytes = []
-        byte = 0
-        for i in mask_address:
-            if i != ".":
-                address_bytes.append(i)
+            mask_address = input("Enter the mask \n") + "."
+            address_bytes = []
+            byte = 0
+            for i in mask_address:
+                if i != ".":
+                    address_bytes.append(i)
 
-            elif i == ".": 
-                address_bytes[byte::1] = [''.join(address_bytes[byte::1])]
-                byte = byte + 1
-                address_bytes.append("")
+                elif i == ".": 
+                    address_bytes[byte::1] = [''.join(address_bytes[byte::1])]
+                    byte = byte + 1
+                    address_bytes.append("")
 
-        address_bytes.pop()
-
-        for i in address_bytes:
-            i = int(i)
-            address_bytes.append(i)
-            if len(address_bytes) == 8:
-                break
-
-        address_bytes.reverse()
-
-        for i in range(0, 4):
             address_bytes.pop()
 
-        address_bytes.reverse()
+            for i in range(0, len(address_bytes)):
+                address_bytes[i] = int(address_bytes[i])
+
+            #---------------------------------------------------------
+            for i in range(0, len(address_bytes)):
+                address_bytes[i] = decimal_to_binary(address_bytes[i])
+
+            # for i in range(0, len(address_bytes)):
+            #     address_bytes[i] = int(address_bytes[i])
+            # print(address_bytes)
+
+            #-------------------------------------------------------
+            
+            zeros_count = 0
+            index = 0
+            for i in address_bytes:
+                if i == '0':
+                    zeros_count = zeros_count + 8
+                elif i != '0':
+                    zeros_count = zeros_count + address_bytes[index].count('0')
+                    index = index + 1
+            print(zeros_count)
 
 
+        elif which_conv.startswith(('h', 'H')):
+            number_of_hosts = input("How many hosts will you use \n")
+            print(which_conv)
+            print(number_of_hosts)
 
-        #---------------------------------------------------------
-
-
-
-
-        for i in address_bytes:
-            i = decimal_to_binary(int(i))
-            #if i == 0 or '0':
-                #i = "00000000"
-                address_bytes.append(i)
-                continue
-            address_bytes.append(i)
-            if len(address_bytes) == 8:
-                break
-
-        address_bytes.reverse()
-
-        for i in range(0, 4):
-            address_bytes.pop()
-
-        address_bytes.reverse()
-
-        print(address_bytes)
-
-        #-------------------------------------------------------
-        # zeros_count = 0
-
-        # for i in address_bytes:
-        #     if i == "0":
-        #         zeros_count = zeros_count + 8
-        #     elif i != "0":
-        #         zeros_count = zeros_count + address_bytes.count("0")
-
-        # print(zeros_count)
-
-
-
-    elif which_conv.startswith(('h', 'H')):
-        number_of_hosts = input("How many hosts will you use \n")
-        print(which_conv)
-        print(number_of_hosts)
-
-    else:
-        print("Please select a correct option \n")
+        else:
+            print("Please select a correct option \n")
 
 
 if __name__ == '__main__':
